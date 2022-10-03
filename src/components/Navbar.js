eaeb4e24a52669ff03d4460c918d288fc365a68e
Jannik Uekoetter <uekoetter@protonmail.com>
2022-10-03 16:51:47 +0200

    add several fa icons to the project

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled  from 'styled-components'
 import { Link } from 'react-router-dom';
-
+import { FaHome, FaHeart, FaVial } from 'react-icons/fa';
 
 export default function Navbar() {
   return (
@@ -11,13 +11,13 @@ export default function Navbar() {
         <nav>
           <ul style={{ listStyle: 'none', display: 'flex', flexWrap: 'wrap', justifyContent: 'space-around' }}>
             <li>
-              <Link to="/" style={{ width: 100, height: 50}}>Home</Link>
+              <Link to="/" style={{ width: 100, height: 50}}><FaHome  size="4em"/></Link>
             </li>
             <li>
-              <Link to="favorites" style={{ width: 100, height: 50}}>Favorites</Link>
+              <Link to="favorites" style={{ width: 100, height: 50}}><FaHeart  size="4em"/></Link>
             </li>
             <li>
-              <Link to="random" style={{ width: 100, height: 50}}>Random</Link>
+              <Link to="random" style={{ width: 100, height: 50}}><FaVial  size="4em"/></Link>
             </li>
             <div>
    
